b5f37650ff9a1bd6a5132b9d4df0169ac79d6110
Oksana Moroz <k.moroz.mail@gmail.com>
2023-09-17 20:37:37 +0300

    added my CV

--- app.js
+++ app.js
@@ -11,3 +11,21 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+    
+  const downloadButton = document.querySelector(".main-btn");
+
+  downloadButton.addEventListener("click", () => {
+    const resumeUrl =
+      "https://github.com/OkMoroz/CV-Oksana-Moroz/raw/main/CV%20Oksana%20Moroz%20FE.pdf";
+
+    const downloadLink = document.createElement("a");
+    downloadLink.href = resumeUrl;
+    downloadLink.download = "CV Oksana Moroz FE.pdf";
+
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+
+    document.body.removeChild(downloadLink);
+  });
+
